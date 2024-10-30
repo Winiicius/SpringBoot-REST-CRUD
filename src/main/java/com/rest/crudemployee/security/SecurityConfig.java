5ed78282b809e9aa7855e8c909b68ec411ec03e9
Winicius Alexandre <winiciusalexandre@gmail.com>
2024-10-30 15:39:59 -0300

    Adicionando cargos e usuários diretamente no banco de dados

--- src/main/java/com/rest/crudemployee/security/SecurityConfig.java
+++ src/main/java/com/rest/crudemployee/security/SecurityConfig.java
@@ -1,42 +1,45 @@
 package com.rest.crudemployee.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
 
-        UserDetails john = User.builder()
-                            .username("john")
-                            .password("{noop}test123")
-                            .roles("EMPLOYEE")
-                            .build();
+    // Código não mais necessário, pois os usuários e seus cargos, agora são definidos no próprio banco de dados
+    // @Bean
+    // public InMemoryUserDetailsManager userDetailsManager(){
 
-        UserDetails mary = User.builder()
-                            .username("mary")
-                            .password("{noop}test123")
-                            .roles("EMPLOYEE", "MANAGER")
-                            .build();
+    //     UserDetails john = User.builder()
+    //                         .username("john")
+    //                         .password("{noop}test123")
+    //                         .roles("EMPLOYEE")
+    //                         .build();
+
+    //     UserDetails mary = User.builder()
+    //                         .username("mary")
+    //                         .password("{noop}test123")
+    //                         .roles("EMPLOYEE", "MANAGER")
+    //                         .build();
         
-        UserDetails susan = User.builder()
-                            .username("susan")
-                            .password("{noop}test123")
-                            .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                            .build();
+    //     UserDetails susan = User.builder()
+    //                         .username("susan")
+    //                         .password("{noop}test123")
+    //                         .roles("EMPLOYEE", "MANAGER", "ADMIN")
+    //                         .build();
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    //     return new InMemoryUserDetailsManager(john, mary, susan);
 
-    }
+    // }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -55,5 +58,10 @@ public class SecurityConfig {
 
         return http.build();
 
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 }
